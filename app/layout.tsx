f7394bc188db89410721b71ed9210b34a46bd915
JiruGutema <jirudagutema@gmail.com>
2025-07-19 13:40:40 +0300

    favicon is changed to png

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Jiru Gutema | Software Engineer & Fullstack Developer Portfolio",
   icons: {
-    icon: "/favicon.ico",
+    icon: "/icon.png",
   },
   description:
     "The official online portfolio of Jiru Gutema, a software engineer and fullstack developer student showcasing projects, skills, and experience.",
